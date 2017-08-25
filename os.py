5ab9082b70b32c6c5463133e27fcdf49c1529fe6
kangbo <lala.kang1989@gmail.com>
2017-08-25 16:07:13 +0800

    OS模块 暂时没用到

---
+++ os.py
@@ -0,0 +1,17 @@
+# _*_coding: utf-8 _*_
+#文件读写操作
+
+import os
+print 'osname: %s \n osgetcwd: %s' % (os.name, os.getcwd)
+
+
+#os.getenv()和os.putenv()函数分别用来读取和设置环境变量。
+#os.listdir()返回指定目录下的所有文件和目录名。
+
+#os.remove()函数用来删除一个文件。
+
+#os.system()函数用来运行shell命令。
+
+#os.linesep字符串给出当前平台使用的行终止符。例如，Windows使用'\r\n'，Linux使用'\n'而Mac使用'\r'。
+
+#os.path.split()函数返回一个路径的目录名和文件名。
