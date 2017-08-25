33a3504a382155ae5385bad2295a1c88a26c7c67
kangbo <lala.kang1989@gmail.com>
2017-08-25 10:27:00 +0800

    fix class_init

--- class_init.py
+++ class_init.py
@@ -3,11 +3,11 @@
 #__init__是个初始化方法
 
 class Person():
-	"""docstring for name"""
 	def __init__(self, name):
 		self.name = name
-		def sayHi(self):
-			print 'hello my name is ', self.name
+		print '初始化就执行这个'
+	def sayHi(self):
+		print 'hello my name is ', self.name
 
 
 
