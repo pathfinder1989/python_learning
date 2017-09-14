402a4f70fc773e4703c3f8d3f110aa4b7718911a
kangbo <lala.kang1989@gmail.com>
2017-09-14 21:48:46 +0800

    class添加方法

--- class_stu.py
+++ class_stu.py
@@ -8,9 +8,19 @@ class student(object):
 		print('%s : %s' % (self.name , self.score))
 		
 
+	def get_grade(self):
+		if self.score >= 90:
+			return 'A'
+		elif self.score >= 60:
+			return 'B'
+		else:
+			return 'C'
+
 
 bart = student('kang', 59)
 lisa = student('bo', 60)
 
 bart.print_score()
 lisa.print_score()
+
+lisa.get_grade()
